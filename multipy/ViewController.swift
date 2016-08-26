c4cf61f7668bb5f098f1ddce55743b21a04b84c6
Kai Gorman <gormankai@Kais-MacBook-Pro.local>
2016-08-26 09:21:08 -0700

    finished app

--- multipy/ViewController.swift
+++ multipy/ViewController.swift
@@ -10,26 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlets
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var startBtn: UIButton!
     @IBOutlet weak var typeNumberTxt: UITextField!
     
     @IBOutlet weak var addBtn: UIButton!
-    @IBOutlet weak var numberLbl: UILabel!
-
-    @IBAction func whenStartBtnPressed(sender: UIButton) {
+    @IBOutlet weak var numbersLbl: UILabel!
+    
+    //Properties
+    var multiple = 0
+    var runningSum = 0
+    let MAX_SUM = 50
+    
+    @IBAction func onStartBtnPressed(sender: UIButton) {
         if typeNumberTxt.text != nil && typeNumberTxt.text != "" {
             
-            logoImg.hidden = false
-            startBtn.hidden = false
-            typeNumberTxt.hidden = false
+            logoImg.hidden = true
+            startBtn.hidden = true
+            typeNumberTxt.hidden = true
             
-            addBtn.hidden = true
-            numberLbl.hidden = true
+            addBtn.hidden = false
+            numbersLbl.hidden = false
+            
+            resetLbl()
+            multiple = Int(typeNumberTxt.text!)!
+            runningSum = 0 
+        }
+        
     }
-}
+    
+    func updateLbl( oldSum: Int, mul: Int, newSum: Int) {
+        numbersLbl.text = "\(oldSum) + \(mul) = \(newSum)"
+    }
+    
+    @IBAction func onAddBtnPressed(sender: UIButton) {
+        let newSum = runningSum + multiple
+        updateLbl(runningSum, mul: multiple, newSum: newSum)
+        runningSum += multiple
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
+    func resetLbl() {
+        numbersLbl.text = "Press Add to Add!"
+    }
+    
+    func restartGame() {
+        multiple = 0
+        logoImg.hidden = false
+        startBtn.hidden = false
+        typeNumberTxt.hidden = false
+        
+        addBtn.hidden = true
+        numbersLbl.hidden = true
+        resetLbl()
+        
+    }
+    
+    func isGameOver() -> Bool {
+        if runningSum >= MAX_SUM {
+            return true
+        } else {
+            return false
+        }
+    
+    
 
 }
+}
+
 
 
 
